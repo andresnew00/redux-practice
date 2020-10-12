901d7100e2eee908463ec16280a3d307ac563a41
Andres Inciarte <you@example.com>
2020-10-12 14:12:24 -0400

    initial commit for react app redux practice

---
+++ redux-practice/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Posts from "./components/Posts";
+import PostForm from "./components/PostForm";
+
+function App() {
+  return (
+    <div className="App">
+      <PostForm />
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
